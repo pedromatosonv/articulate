cc534197e3e88c4009240c52d43f1f0b3080a0ab
Pedro Matoso <pedroomatoso@gmail.com>
2026-05-08 21:09:03 -0300

    Remove toolbar zoom controls

--- Sources/ArticulateApp/Views/ContentView.swift
+++ Sources/ArticulateApp/Views/ContentView.swift
@@ -1,38 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject private var store: PracticeStore
-
     var body: some View {
         NavigationSplitView {
             SidebarView()
                 .navigationSplitViewColumnWidth(min: 280, ideal: 310, max: 360)
         } detail: {
             PracticeDetailView()
-        }
-        .toolbar {
-            ToolbarItemGroup {
-                Button {
-                    store.zoomOut()
-                } label: {
-                    Label("Zoom Out", systemImage: "minus.magnifyingglass")
-                }
-                .disabled(!store.canZoomOut)
-
-                Button {
-                    store.resetZoom()
-                } label: {
-                    Label(store.contentScaleLabel, systemImage: "textformat.size")
-                }
-                .help("Reset Zoom")
-
-                Button {
-                    store.zoomIn()
-                } label: {
-                    Label("Zoom In", systemImage: "plus.magnifyingglass")
-                }
-                .disabled(!store.canZoomIn)
-            }
         }
     }
 }
